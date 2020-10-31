39b2bc28c6ede97897ea12ca4c08f2cd9cb6ec70
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-31 18:06:41 -0500

    Display current weather info

--- assets/weatherData_handler.js
+++ assets/weatherData_handler.js
@@ -1,5 +1,11 @@
 var search= document.querySelector("#search-form");
 var cityName= document.querySelector("#city");
+var currCity= document.querySelector(".city");
+var today= document.querySelector("#date");
+var temperature= document.getElementById("temp");
+var humidity= document.getElementById("humid");
+var wind= document.getElementById('mph');
+var UV= document.getElementById('uv');
 var id= "6816f64f60a3280d741d1bb4d4f17625";
 
 var getCity= function(event){
@@ -9,6 +15,7 @@ var getCity= function(event){
     var CurrUrl= "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid="+id;
     fetch(CurrUrl).then(function(response){
         response.json().then(function(data){
+            currCity.innerHTML= city;
             displayCurrWeather(data);
         });
     });
@@ -17,9 +24,16 @@ var getCity= function(event){
 function displayCurrWeather(forecast){
     var lati= forecast.coord.lat;
     var long= forecast.coord.lon;
-    var fURL= "https://api.openweathermap.org/data/2.5/onecall?lat="+lati+"&lon="+long+"&exclude=alerts,hourly,minutely,current&units=imperial&appid="+id;
+    var currSec= forecast.dt;
+    var date= new Date(currSec * 1000);
+    today.innerHTML= 1+date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear();
+    temperature.innerHTML= forecast.main.temp;
+    humidity.innerHTML= forecast.main.humidity;
+    wind.innerHTML= forecast.wind.speed;
+    var fURL= "https://api.openweathermap.org/data/2.5/onecall?lat="+lati+"&lon="+long+"&exclude=alerts,hourly,minutely&units=imperial&appid="+id;
     fetch(fURL).then(function(response){
         response.json().then(function(data){
+            UV.innerHTML= data.current.uvi;
             displayFutureWeather(data);
         });
     });
